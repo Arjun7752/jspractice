e37915ea1d8506ffb2c672170421cee1dd45ece8
Arjun7752 <82261529+Arjun7752@users.noreply.github.com>
2023-03-30 17:49:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ function getMaxLength3(arr){
 
 //console.log([1, 1,1,1,1,1,1,1,1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1]);
 //output : 9
+
+function findMiniMax(arr){ 
+    let mini = arr[0];
+    let maxi = arr[0];
+
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] < mini){
+            mini = arr[i];
+        } else if(arr[i] > maxi){
+            maxi = arr[i];
+        }
+    }
+
+    let ans = {
+        "mini": mini,
+        "max": maxi
+    }
+
+    return ans;
+    
+}
+//findMiniMax([9,7,1,0,4,2]);
+//ouput: {mini: 0, max: 9}
