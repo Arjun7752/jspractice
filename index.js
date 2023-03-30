773f444f77997d52abc20a3e54a6d126b62ec133
Arjun7752 <82261529+Arjun7752@users.noreply.github.com>
2023-03-30 17:05:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+//Maximum consecutive 1's in a binary array
 
-console.log('test ...')
+function getMaxLength3(arr){
+    let count = 0;
+    let result = 0;
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] == 0){
+            count = 0;
+        } else {
+            count++;
+            result = Math.max(result, count);
+        }
+    }
+    return result; 
+}
+
+//console.log([1, 1,1,1,1,1,1,1,1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1]);
+//output : 9
