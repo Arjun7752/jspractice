6c7e3a4a3ee8284abee3bda27c727060453e1d12
Arjun7752 <82261529+Arjun7752@users.noreply.github.com>
2023-05-14 04:50:08 +0530

    Create missingArrElement.js

---
+++ missingArrElement.js
@@ -0,0 +1,10 @@
+//missing array elements
+function missingArrElement(arr) {
+  let miss= arr.sort( (a,b) => a > b ? 1 : -1 );
+  let result = [];
+  for(let i = Math.min(...miss); i < Math.max(...miss); i++) {
+    if(miss.indexOf(i) < 0) {
+      resutl.push(i)
+    }
+  }
+}
